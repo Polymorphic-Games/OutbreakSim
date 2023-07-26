fec48332ac5fb52ce8091b907eb88090a368f81d
CJ5518 <carsonrueber@gmail.com>
2023-07-25 17:20:17 -0700

    Add basic framework for simulation

---
+++ Assets/Scripts/Simulation/DiseaseState.cs
@@ -0,0 +1,9 @@
+
+//Holds the disease state of a single cell
+
+namespace CJSim {
+    class DiseaseState {
+        int[] state;
+        
+    }
+}
---
+++ Assets/Scripts/Simulation/SimCore.cs
@@ -0,0 +1,10 @@
+
+
+//Holds the core of the simulation
+//Operates on an array of cells and handles movement between them
+
+namespace CJSim {
+    class SimCore {
+
+    }
+}
